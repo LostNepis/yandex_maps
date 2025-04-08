281056d6f0935154fe870e5241829bf5a49e7c6d
Дмитрий <dostupniy.nic@gmail.com>
2025-04-09 00:32:10 +0700

    дополнил функуионал

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from PIL import Image
+from io import BytesIO
 
 API_KEY = 'fd68f2be-2c3a-42c3-ab7d-190007a9515c'
 SEARCH_URL = 'https://search-maps.yandex.ru/v1/'
@@ -19,6 +21,22 @@ feature = json_response["features"][0]
 company = feature["properties"]["CompanyMetaData"]
 name = company.get("name", "Без названия")
 address = company.get("address", "Адрес не указан")
+coords = feature["geometry"]["coordinates"]
 
 print(name)
 print(address)
+
+map_params = {
+        "ll": f"{coords[0]},{coords[1]}",
+        "size": "450,450",
+        "z": "16",
+        "l": "map",
+        "pt": f"{coords[0]},{coords[1]},pm2rdl"
+    }
+map_response = requests.get(STATIC_MAP_URL, params=map_params)
+
+if map_response:
+    image = Image.open(BytesIO(map_response.content))
+    image.show()
+else:
+    print("Не удалось получить карту.")
